45f4016e4b853fcfeecb2b22cd4ab2e20cade944
sm110101 <seanmorris17@gmail.com>
2025-03-26 16:54:45 -0400

    fix attribute error in ap.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from main import process_and_return_gif, load_model
 
 def handle_video_upload(video):
     with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as temp_vid:
-        temp_vid.write(video.read())
-        temp_vid.flush()
+       # temp_vid.write(video.read())
+       # temp_vid.flush()
         gif = process_and_return_gif(temp_vid.name, model=load_model())
         return gif
 
