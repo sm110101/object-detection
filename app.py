1850a59cf50d3576e1300bed903b313cef9341f0
sm110101 <seanmorris17@gmail.com>
2025-03-26 16:49:38 -0400

    fix model reference in app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import gradio as gr
 from ultralytics import YOLO
 from pathlib import Path
 import tempfile
-from main import process_and_return_gif
+from main import process_and_return_gif, load_model
 
 def handle_video_upload(video):
     with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as temp_vid:
         temp_vid.write(video.read())
         temp_vid.flush()
-        gif = process_and_return_gif(temp_vid.name, model)
+        gif = process_and_return_gif(temp_vid.name, model=load_model())
         return gif
 
 gr.Interface(
